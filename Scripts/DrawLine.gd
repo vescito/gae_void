3ae2bb9be2053a1ed72d25d2e0720d20aea8130e
Niels Kochert <48764692+vescito@users.noreply.github.com>
2024-05-15 13:17:39 +0200

    arrays mit lines

--- Scripts/DrawLine.gd
+++ Scripts/DrawLine.gd
@@ -5,6 +5,10 @@ var isInArea = false
 var currentArea : String
 var previousArea : String
 
+var array1 = []
+var array2 = []
+var array3 = []
+
 
 func _input(event):
 
@@ -18,7 +22,7 @@ func _input(event):
 					print("Current area is: " + currentArea)
 					print("Previous area is: " + previousArea)
 					if currentArea != previousArea:
-						createLine(from_position, get_local_mouse_position())
+						createLine(from_position, get_local_mouse_position(), currentArea, previousArea)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -28,10 +32,16 @@ func _ready():
 func _process(delta):
 	pass
 
-func createLine(from, to):
+func createLine(from, to, currentArea, previousArea):
 	var new_line = Line2D.new()
 	new_line.points = [from, to]
 	add_child(new_line)
+	if previousArea == "Area2D_1" || currentArea == "Area2D_1":
+		array1.append(new_line)
+	elif previousArea == "Area2D_4" || currentArea == "Area2D_4":
+		array3.append(new_line)
+	elif (previousArea == "Area2D_3" && currentArea == "Area2D_2") || (previousArea == "Area2D_2" && currentArea == "Area2D_3"):
+		array2.append(new_line)
 
 func _on_area_2d_1_mouse_entered(extra_arg_0):
 	print("Entered at: " + extra_arg_0)
