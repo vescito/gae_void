dccf6bebc02a575ff75221d862d27e5c6ffdbcdc
Niels Kochert <48764692+vescito@users.noreply.github.com>
2024-05-15 18:02:03 +0200

    wohooo progress

--- Scripts/DrawLine.gd
+++ Scripts/DrawLine.gd
@@ -25,7 +25,16 @@ func _input(event):
 						createLine(from_position, get_local_mouse_position(), currentArea, previousArea)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var area1 = %Area1
+	var area2 = %Area2
+	var area3 = %Area3
+	for child in area1.get_children():
+		array1.append(child)
+	for child in area2.get_children():
+		array2.append(child)
+	for child in area3.get_children():
+		array3.append(child)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,15 +42,25 @@ func _process(delta):
 	pass
 
 func createLine(from, to, currentArea, previousArea):
-	var new_line = Line2D.new()
-	new_line.points = [from, to]
-	add_child(new_line)
-	if previousArea == "Area2D_1" || currentArea == "Area2D_1":
-		array1.append(new_line)
-	elif previousArea == "Area2D_4" || currentArea == "Area2D_4":
-		array3.append(new_line)
-	elif (previousArea == "Area2D_3" && currentArea == "Area2D_2") || (previousArea == "Area2D_2" && currentArea == "Area2D_3"):
-		array2.append(new_line)
+	var intersects : bool
+	intersects = false
+	if(currentArea == "Area2D_1" || previousArea == "Area2D_1"):
+		for line in array1:
+			if(Geometry2D.segment_intersects_segment(from, to, line.points[0], line.points[1]) != null):
+				intersects = true
+	print(intersects)
+	if intersects == false:
+		var new_line = Line2D.new()
+		new_line.points = [from, to]
+		add_child(new_line)
+		if previousArea == "Area2D_1" || currentArea == "Area2D_1":
+			array1.append(new_line)
+		elif previousArea == "Area2D_4" || currentArea == "Area2D_4":
+			array3.append(new_line)
+		elif (previousArea == "Area2D_3" && currentArea == "Area2D_2") || (previousArea == "Area2D_2" && currentArea == "Area2D_3"):
+			array2.append(new_line)
+
+
 
 func _on_area_2d_1_mouse_entered(extra_arg_0):
 	print("Entered at: " + extra_arg_0)
