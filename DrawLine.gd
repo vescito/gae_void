b7ae86fc728c1ab3d633a356b1f4a153227b4acf
Niels Kochert <48764692+vescito@users.noreply.github.com>
2024-05-15 10:34:21 +0200

    spawn lines

--- DrawLine.gd
+++ DrawLine.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
 var from_position = null
+var isInArea = false
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			pass
-			#print("a")
-			#if event.pressed:
-				#from_position = get_area_under_mouse()
-			#elif from_position:
-				#var to_position = get_area_under_mouse()
-				#if to_position and to_position != from_position:
-					#line2d.points = [from_position, to_position]
-					#from_position = null  # Reset start position
+	if isInArea == true:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				if event.pressed:
+					from_position = get_viewport().get_mouse_position()
+				else:
+					createLine(from_position, get_local_mouse_position())
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -23,9 +20,15 @@ func _ready():
 func _process(delta):
 	pass
 
-func _on_Area2D_mouse_entered():
-	print("trueaaa")
+func createLine(from, to):
+	var new_line = Line2D.new()
+	new_line.points = [from, to]
+	add_child(new_line)
 	
 func _on_area_2d_mouse_exited():
-	print("SAA")
-	pass # Replace with function body.
+	isInArea = false
+	pass 
+	
+func _on_area_2d_mouse_entered():
+	isInArea = true
+	pass 
